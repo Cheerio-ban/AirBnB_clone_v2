f969433d163d2971c025e9867db2d40a0ea1e9a5
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-01-13 14:04:28 -0500

    C+any string route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Initial flask app"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """Renders return content in the home route"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Renders content in the hbnb route"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """Renders content in the c + any string path route"""
+    return "C {}".format(text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
