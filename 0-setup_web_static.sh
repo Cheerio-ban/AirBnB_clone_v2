87fecd933781fddc5659e3dd624f6f3b0ca9bd2f
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-01-22 08:55:42 -0500

    Serves web content

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
-# sets up your web servers for the deployment of web_static
-apt-get -y update
-apt-get -y install nginx
+
+# Serves web Content
+
+sudo apt-get update
+sudo apt-get install -y nginx
+mkdir -p /data/
 mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current
+mkdir -p /data/web_static/releases/test/
+echo "Hello, World!" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/current /data/web_static/releases/test/
 chown -R ubuntu:ubuntu /data
 sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
-service nginx restart
-exit 0
+sudo systemctl restart nginx
+nginx 0
