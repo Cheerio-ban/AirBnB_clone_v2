a53e1f152d7b890bd1a51f5b28cc42d32713399f
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-01-22 09:04:39 -0500

    Serves web content

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,4 +13,4 @@ ln -sf /data/web_static/current /data/web_static/releases/test/
 chown -R ubuntu:ubuntu /data
 sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 sudo systemctl restart nginx
-nginx 0
+exit 0
