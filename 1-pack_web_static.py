9d715f196f62a17d7a822893a80e598a4eb46212
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-01-23 04:44:16 -0500

    compresses files with tgz

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
 """
-A python fabric script that generates an archive
-from the web static folder using targz
+A fabric script that generates a .tgz archive from the contents of the
+web_static folder of your AirBnB Clone repo, using the function do_pack
 """
 
 from os import path
-from datetime import date
+from datetime import datetime
 from fabric.api import local
 
 
 def do_pack():
     """
-    Embeds the content of the web static directory into
-    an archive created by tgz.
+    A function that generates an archive
     """
     date = datetime.now()
-    archive_dir = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-            date.year, date.month, date.day, day.hour, date.minute
-            date.second)
+    archive = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        date.year, date.month, date.day,
+        date.hour, date.minute, date.second
+    )
     if not path.isdir("versions"):
         if local("mkdir versions").failed:
             return None
-    crt_archive = "cd web_static && tar -cvzf ../{} . \
-            && cd -".format(archive_dir)
-    if local(crt_archive).succeeded:
-        return archive_dir
+    cmd = "cd web_static && tar -cvzf ../{} . && cd -".format(archive)
+    if local(cmd).succeeded:
+        return archive
     return None
